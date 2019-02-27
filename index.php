16166ad94520d8d8717db7bd2be121d9e6f04113
Israelfluz <israelfaria.gtr@gmail.com>
2019-02-27 11:13:26 -0300

    crud

--- index.php
+++ index.php
@@ -31,12 +31,12 @@ $app->get('/admin', function() {
 
 $app->get('/admin/login', function() {
 
-	$page new PageAdmin([
+	$page = new PageAdmin([
 		"header"=>false,
 		"footer"=>false
 	]);
 
-	$page->setTpl("login";);
+	$page->setTpl("login");
 
 });
 
@@ -44,7 +44,7 @@ $app->post('/admin/login', function(){
 
 	User::login($_POST["login"], $_POST["password"]);
 
-	header("Location: /admin");
+	header("Location: /ecommerce/admin");
 	exit;
 
 });
@@ -53,9 +53,103 @@ $app->get('/admin/logout', function(){
 
 	User::logout();
 
-	header("Locatoin: /admin/login");
+	header("Locatoin: /ecommerce/admin/login");
 	exit;
+
 });
+
+$app->get("admin/users", function(){
+
+	User::verifylogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get("admin/users/create", function(){
+
+	User::verifylogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->delete("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifylogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /ecommerce/admin/users");
+	exit;
+
+});
+
+$app->get("admin/users/:iduser", function($iduser){
+
+	User::verifylogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+
+});
+
+$app->post("/admin/users/create", function() {
+
+	User::verifylogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /ecommerce/admin/users");
+	exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	User::verifylogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /ecommerce/admin/users");
+	exit;
+
+});
+
+
 
 $app->run();
 
