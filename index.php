d4f2186abeb8cc7e03e0b27866205211339cafc8
Israelfluz <israelfaria.gtr@gmail.com>
2019-02-28 11:20:15 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -149,7 +149,77 @@ $app->post("/admin/users/:iduser", function($iduser) {
 
 });
 
+$app->get("/admin/forgot", function(){
 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot"); 
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /ecommerce/admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/ecommerce/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-send"); 
+
+});
+
+$app->get("/ecommerce/admin/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	)); 
+
+});
+
+$app->post("/ecommerce/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setforgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+ 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
 
 $app->run();
 
