ba53806b20bb997b93960f99b689043b7bb8c8d1
Israelfluz <israelfaria.gtr@gmail.com>
2019-02-28 17:28:47 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin; 
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -220,6 +221,95 @@ $app->post("/ecommerce/admin/forgot/reset", function(){
 	$page->setTpl("forgot-reset-success");
 
 });
+
+$app->get("/ecommerce/admin/categories", function(){
+
+	User::verifylogin();
+
+	$categories - Category::listAll();
+
+	$page = new PageAdmin();
+	 
+	$page->setTpl("categories",
+		'categories'=>$categories
+
+	]);
+
+});
+
+$app->get("/ecommerce/admin/categories/create", function(){
+
+	User::verifylogin();
+
+	$page = new PageAdmin();
+	 
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/ecommerce/admin/categories/create", function(){
+
+	User::verifylogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /ecommerce/admin/categories');
+	exit;
+
+});
+
+$app->get("/ecommerce/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifylogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /ecommerce/admin/categories');
+	exit;
+
+});
+
+$app->get("/ecommerce/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifylogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+	 
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+}); 
+
+
+$app->post("/ecommerce/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifylogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /ecommerce/admin/categories');
+	exit;
+
+}); 
 
 $app->run();
 
