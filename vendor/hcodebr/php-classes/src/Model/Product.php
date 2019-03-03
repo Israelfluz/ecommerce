8c4ade7c62681ddeb61ee85d24d381ef074d3b24
Israelfluz <israelfaria.gtr@gmail.com>
2019-03-02 22:49:31 -0300

    adicionando vendor

---
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+use \Hcode\Mailer; 
+
+class Product extends Model {
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql ->select("SELECT * FROM tb_product ORDER BY desproduct");
+
+	}
+
+	public static function checklist($list)
+	{
+
+		foreach ($list as &$row) {
+			
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValeus();
+
+		}
+
+		return $list;
+
+	}
+
+	public function save();
+	{
+
+		$sql = new Sql();
+		
+		$results = $sql->select("CALL sp_product_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheigth, :vllength, :vlweigth, :desurl)", array(
+			":idproduct"=>$this->getidproduct(),
+			":desproduct"=>$this->getdesproduct(),
+			":vlprice"=>$this->getvlprice(),
+			":vlwidth"=>$this->getvlwidth(),
+			":vlheigth"=>$this->getvlheigth(),
+			":vllength"=>$this->getvllength(),
+			":vlweigth"=>$this->getvlweigth(),
+			":desurl"=>$this->getdesurl()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function get($idproduct)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$idproduct
+		]);
+
+		$this->setData($results[0]);
+	}
+	
+	public function delete()
+	{
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct",[
+			':idproduct'=>$this->getidproduct()
+		]);
+
+	
+	}
+
+	public function checkPhoto()
+	{
+
+		if (file_exists(
+			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"resource" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+			)) {
+
+			$url = "/ecommercer/resource/site/img/products/" . $this->getidproduct() . ".jpg";
+
+		} else {
+
+			$url = "/ecommercer/resource/site/img/product.jpg";
+		}
+
+		return $this->setdesPhoto($url);
+
+	}
+
+	public function getValeus()
+	{
+
+		$this->checkPhoto();
+
+		$values = parents::getValeus();
+
+		return $values;
+
+	}
+
+	public function setPhoto($file)
+	{
+
+		$extension = explode('.', $file['name']);
+		$extension = end($extension);
+
+		switch ($extension) {
+
+			case "jpg":
+			case "jpeg":
+			$image = imagecreatefromjpeg($file["tmp_name"]);
+			break;
+
+			case "gif":
+			$image = imagecreatefromgif($file["tmp_name"]);
+			break;
+
+			case "png":
+			$image = imagecreatefrompng($file["tmp_name"]);
+			break;
+		
+
+		}
+
+		$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"resource" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+
+		imagejpeg($image, $dist);
+
+		imagedestroy($image);
+
+		$this->checkPhoto();
+
+	}
+
+}
+
+?>
+
