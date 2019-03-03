8bf71ac3ceb9ac8cdf3a857c350c947e8248e3b3
Israelfluz <israelfaria.gtr@gmail.com>
2019-03-02 22:11:49 -0300

    Site - Produtos

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 
-
 session_start();
 require_once("vendor/autoload.php");
 
@@ -9,6 +8,7 @@ $app = new Slim();
 
 $app->config('debug', true);
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
